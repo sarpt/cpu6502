50e9fadc232e031bf0d7d6306c46fd28d1cb4c8e
sarpt <mike.m.info@gmail.com>
2024-02-18 21:26:49 +0100

    Add eor tests

--- cpu6502/src/cpu/instructions/logical/tests.rs
+++ cpu6502/src/cpu/instructions/logical/tests.rs
@@ -1231,3 +1231,546 @@ mod and {
         }
     }
 }
+
+#[cfg(test)]
+mod eor {
+    #[cfg(test)]
+    mod eor_im {
+        use std::{cell::RefCell, rc::Rc};
+
+        use crate::{
+            consts::Byte,
+            cpu::{instructions::eor_im, tests::MemoryMock, CPU},
+        };
+
+        const VALUE: Byte = 0x82;
+
+        #[test]
+        fn should_eor_accumulator_with_a_value_from_immediate_address() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[VALUE, 0x00]))));
+            cpu.program_counter = 0x00;
+            cpu.accumulator = 0x07;
+
+            eor_im(&mut cpu);
+
+            assert_eq!(cpu.accumulator, 0x02);
+        }
+
+        #[test]
+        fn should_set_processor_status() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[VALUE, 0x00]))));
+            cpu.program_counter = 0x00;
+            cpu.accumulator = 0x86;
+
+            eor_im(&mut cpu);
+
+            assert_eq!(cpu.processor_status, 0x80);
+        }
+
+        #[test]
+        fn should_take_one_cycle() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[VALUE, 0x00]))));
+            cpu.program_counter = 0x00;
+            cpu.accumulator = 0x07;
+            cpu.cycle = 0;
+
+            eor_im(&mut cpu);
+
+            assert_eq!(cpu.cycle, 1);
+        }
+    }
+
+    #[cfg(test)]
+    mod eor_zp {
+        use std::{cell::RefCell, rc::Rc};
+
+        use crate::cpu::{instructions::eor_zp, tests::MemoryMock, Byte, CPU};
+
+        const ZERO_PAGE_ADDR: Byte = 0x03;
+        const VALUE: Byte = 0x82;
+
+        #[test]
+        fn should_eor_accumulator_with_a_value_from_zero_page_address() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ZERO_PAGE_ADDR,
+                0xFF,
+                0x00,
+                VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.program_counter = 0x00;
+
+            eor_zp(&mut cpu);
+
+            assert_eq!(cpu.accumulator, 0x02);
+        }
+
+        #[test]
+        fn should_set_processor_status() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ZERO_PAGE_ADDR,
+                0xFF,
+                0x00,
+                VALUE,
+            ]))));
+            cpu.accumulator = 0x86;
+            cpu.program_counter = 0x00;
+
+            eor_zp(&mut cpu);
+
+            assert_eq!(cpu.processor_status, 0x80);
+        }
+
+        #[test]
+        fn should_take_two_cycles() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ZERO_PAGE_ADDR,
+                0xFF,
+                0x00,
+                VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.program_counter = 0x00;
+            cpu.cycle = 0;
+
+            eor_zp(&mut cpu);
+
+            assert_eq!(cpu.cycle, 2);
+        }
+    }
+
+    #[cfg(test)]
+    mod eor_zpx {
+        use std::{cell::RefCell, rc::Rc};
+
+        use crate::cpu::{instructions::eor_zpx, tests::MemoryMock, Byte, CPU};
+
+        const ZERO_PAGE_ADDR: Byte = 0x01;
+        const VALUE: Byte = 0x82;
+
+        #[test]
+        fn should_eor_accumulator_with_a_value_at_a_zero_page_address_summed_with_index_register_x()
+        {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ZERO_PAGE_ADDR,
+                0xFF,
+                0x00,
+                VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.index_register_x = 0x02;
+            cpu.program_counter = 0x00;
+
+            eor_zpx(&mut cpu);
+
+            assert_eq!(cpu.accumulator, 0x02);
+        }
+
+        #[test]
+        fn should_set_processor_status() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ZERO_PAGE_ADDR,
+                0xFF,
+                0x00,
+                VALUE,
+            ]))));
+            cpu.accumulator = 0x86;
+            cpu.index_register_x = 0x02;
+            cpu.program_counter = 0x00;
+
+            eor_zpx(&mut cpu);
+
+            assert_eq!(cpu.processor_status, 0x80);
+        }
+
+        #[test]
+        fn should_take_three_cycles() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ZERO_PAGE_ADDR,
+                0xFF,
+                0x00,
+                VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.index_register_x = 0x02;
+            cpu.program_counter = 0x00;
+            cpu.cycle = 0;
+
+            eor_zpx(&mut cpu);
+
+            assert_eq!(cpu.cycle, 3);
+        }
+    }
+
+    #[cfg(test)]
+    mod eor_a {
+        use std::{cell::RefCell, rc::Rc};
+
+        use crate::cpu::{instructions::eor_a, tests::MemoryMock, Byte, CPU};
+
+        const ADDR_LO: Byte = 0x04;
+        const ADDR_HI: Byte = 0x00;
+        const VALUE: Byte = 0x82;
+
+        #[test]
+        fn should_eor_accumulator_with_a_value_in_memory_at_an_absolute_address() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ADDR_LO, ADDR_HI, 0x00, 0x00, VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.program_counter = 0x00;
+
+            eor_a(&mut cpu);
+
+            assert_eq!(cpu.accumulator, 0x02);
+        }
+
+        #[test]
+        fn should_set_processor_status() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ADDR_LO, ADDR_HI, 0x00, 0x00, VALUE,
+            ]))));
+            cpu.accumulator = 0x86;
+            cpu.program_counter = 0x00;
+
+            eor_a(&mut cpu);
+
+            assert_eq!(cpu.processor_status, 0x80);
+        }
+
+        #[test]
+        fn should_take_three_cycles() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ADDR_LO, ADDR_HI, 0x00, 0x00, VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.program_counter = 0x00;
+            cpu.cycle = 0;
+
+            eor_a(&mut cpu);
+
+            assert_eq!(cpu.cycle, 3);
+        }
+    }
+
+    #[cfg(test)]
+    mod eor_ax {
+        use std::{cell::RefCell, rc::Rc};
+
+        use crate::cpu::{instructions::eor_ax, tests::MemoryMock, Byte, CPU};
+
+        const ADDR_LO: Byte = 0x02;
+        const ADDR_HI: Byte = 0x00;
+        const OFFSET: Byte = 0x02;
+        const VALUE: Byte = 0x82;
+        const ADDR_LO_ON_ZERO_PAGE_BOUNDARY: Byte = 0xFF;
+
+        #[test]
+        fn should_eor_accumulator_with_a_value_in_memory_at_an_absolute_address_offset_by_index_register_x(
+        ) {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ADDR_LO, ADDR_HI, 0x00, 0x00, VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.program_counter = 0x00;
+            cpu.index_register_x = OFFSET;
+
+            eor_ax(&mut cpu);
+
+            assert_eq!(cpu.accumulator, 0x02);
+        }
+
+        #[test]
+        fn should_set_processor_status() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ADDR_LO, ADDR_HI, 0x00, 0x00, VALUE,
+            ]))));
+            cpu.accumulator = 0x86;
+            cpu.program_counter = 0x00;
+            cpu.index_register_x = OFFSET;
+
+            eor_ax(&mut cpu);
+
+            assert_eq!(cpu.processor_status, 0x80);
+        }
+
+        #[test]
+        fn should_take_three_cycles_when_adding_offset_crosses_over_page_flip() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ADDR_LO, ADDR_HI, 0x00, 0x00, VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.program_counter = 0x00;
+            cpu.index_register_x = OFFSET;
+            cpu.cycle = 0;
+
+            eor_ax(&mut cpu);
+
+            assert_eq!(cpu.cycle, 3);
+        }
+
+        #[test]
+        fn should_take_four_cycles_when_adding_offset_crosses_over_page_flip() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ADDR_LO_ON_ZERO_PAGE_BOUNDARY,
+                ADDR_HI,
+                0x45,
+                0xAF,
+                0xDD,
+                VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.program_counter = 0x00;
+            cpu.index_register_x = OFFSET;
+            cpu.cycle = 0;
+
+            eor_ax(&mut cpu);
+
+            assert_eq!(cpu.cycle, 4);
+        }
+    }
+
+    #[cfg(test)]
+    mod eor_ay {
+        use std::{cell::RefCell, rc::Rc};
+
+        use crate::cpu::{instructions::eor_ay, tests::MemoryMock, Byte, CPU};
+
+        const ADDR_LO: Byte = 0x02;
+        const ADDR_HI: Byte = 0x00;
+        const OFFSET: Byte = 0x02;
+        const VALUE: Byte = 0x82;
+        const ADDR_LO_ON_ZERO_PAGE_BOUNDARY: Byte = 0xFF;
+
+        #[test]
+        fn should_eor_accumulator_with_a_value_in_memory_at_an_absolute_address_offset_by_index_register_y(
+        ) {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ADDR_LO, ADDR_HI, 0x00, 0x00, VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.program_counter = 0x00;
+            cpu.index_register_y = OFFSET;
+
+            eor_ay(&mut cpu);
+
+            assert_eq!(cpu.accumulator, 0x02);
+        }
+
+        #[test]
+        fn should_set_processor_status() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ADDR_LO, ADDR_HI, 0x00, 0x00, VALUE,
+            ]))));
+            cpu.accumulator = 0x86;
+            cpu.program_counter = 0x00;
+            cpu.index_register_y = OFFSET;
+
+            eor_ay(&mut cpu);
+
+            assert_eq!(cpu.processor_status, 0x80);
+        }
+
+        #[test]
+        fn should_take_three_cycles_when_adding_offset_crosses_over_page_flip() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ADDR_LO, ADDR_HI, 0x00, 0x00, VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.program_counter = 0x00;
+            cpu.index_register_y = OFFSET;
+            cpu.cycle = 0;
+
+            eor_ay(&mut cpu);
+
+            assert_eq!(cpu.cycle, 3);
+        }
+
+        #[test]
+        fn should_take_four_cycles_when_adding_offset_crosses_over_page_flip() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ADDR_LO_ON_ZERO_PAGE_BOUNDARY,
+                ADDR_HI,
+                0x45,
+                0xAF,
+                0xDD,
+                VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.program_counter = 0x00;
+            cpu.index_register_y = OFFSET;
+            cpu.cycle = 0;
+
+            eor_ay(&mut cpu);
+
+            assert_eq!(cpu.cycle, 4);
+        }
+    }
+
+    #[cfg(test)]
+    mod eor_inx {
+        use std::{cell::RefCell, rc::Rc};
+
+        use crate::cpu::{instructions::eor_inx, tests::MemoryMock, Byte, CPU};
+
+        const ZP_ADDRESS: Byte = 0x02;
+        const OFFSET: Byte = 0x01;
+        const EFFECTIVE_ADDRESS_LO: Byte = 0x05;
+        const EFFECTIVE_ADDRESS_HI: Byte = 0x00;
+        const VALUE: Byte = 0x82;
+
+        #[test]
+        fn should_eor_accumulator_with_a_value_in_an_indirect_adress_stored_in_zero_page_offset_with_index_register_x(
+        ) {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ZP_ADDRESS,
+                0x00,
+                0x00,
+                EFFECTIVE_ADDRESS_LO,
+                EFFECTIVE_ADDRESS_HI,
+                VALUE,
+            ]))));
+            cpu.program_counter = 0x00;
+            cpu.accumulator = 0x07;
+            cpu.index_register_x = OFFSET;
+
+            oer_inx(&mut cpu);
+
+            assert_eq!(cpu.accumulator, 0x02);
+        }
+
+        #[test]
+        fn should_set_processor_status() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ZP_ADDRESS,
+                0x00,
+                0x00,
+                EFFECTIVE_ADDRESS_LO,
+                EFFECTIVE_ADDRESS_HI,
+                VALUE,
+            ]))));
+            cpu.program_counter = 0x00;
+            cpu.accumulator = 0x86;
+            cpu.index_register_x = OFFSET;
+
+            eor_inx(&mut cpu);
+
+            assert_eq!(cpu.processor_status, 0x80);
+        }
+
+        #[test]
+        fn should_take_five_cycles() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                ZP_ADDRESS,
+                0x00,
+                0x00,
+                EFFECTIVE_ADDRESS_LO,
+                EFFECTIVE_ADDRESS_HI,
+                VALUE,
+            ]))));
+            cpu.program_counter = 0x00;
+            cpu.accumulator = 0x07;
+            cpu.index_register_x = OFFSET;
+            cpu.cycle = 0;
+
+            eor_inx(&mut cpu);
+
+            assert_eq!(cpu.cycle, 5);
+        }
+    }
+
+    #[cfg(test)]
+    mod eor_iny {
+        use std::{cell::RefCell, rc::Rc};
+
+        use crate::{
+            consts::Byte,
+            cpu::{instructions::eor_iny, tests::MemoryMock, CPU},
+        };
+
+        const INDIRECT_ZERO_PAGE_ADDRESS_PLACE: Byte = 0x01;
+        const ADDRESS_LO: Byte = 0x03;
+        const ADDRESS_LO_ON_ZERO_PAGE_BOUNDARY: Byte = 0xFF;
+        const ADDRESS_HI: Byte = 0x00;
+        const VALUE: Byte = 0x82;
+
+        #[test]
+        fn should_eor_accumulator_with_a_value_from_an_indirect_adress_stored_in_memory_at_zero_page_and_offset_with_value_from_index_register_y(
+        ) {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                INDIRECT_ZERO_PAGE_ADDRESS_PLACE,
+                ADDRESS_LO,
+                ADDRESS_HI,
+                0x45,
+                0xAF,
+                VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.index_register_y = 0x02;
+            cpu.program_counter = 0x00;
+
+            eor_iny(&mut cpu);
+
+            assert_eq!(cpu.accumulator, 0x02);
+        }
+
+        #[test]
+        fn should_set_processor_status() {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                INDIRECT_ZERO_PAGE_ADDRESS_PLACE,
+                ADDRESS_LO,
+                ADDRESS_HI,
+                0x45,
+                0xAF,
+                VALUE,
+            ]))));
+            cpu.accumulator = 0x86;
+            cpu.index_register_y = 0x02;
+            cpu.program_counter = 0x00;
+
+            eor_iny(&mut cpu);
+
+            assert_eq!(cpu.processor_status, 0x80);
+        }
+
+        #[test]
+        fn should_take_four_cycles_when_summing_indirect_address_with_index_y_does_not_cross_page_flip(
+        ) {
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&[
+                INDIRECT_ZERO_PAGE_ADDRESS_PLACE,
+                ADDRESS_LO,
+                ADDRESS_HI,
+                0x45,
+                0xAF,
+                VALUE,
+            ]))));
+            cpu.accumulator = 0x07;
+            cpu.index_register_y = 0x02;
+            cpu.program_counter = 0x00;
+            cpu.cycle = 0;
+
+            eor_iny(&mut cpu);
+
+            assert_eq!(cpu.cycle, 4);
+        }
+
+        #[test]
+        fn should_take_five_cycles_when_summing_indirect_address_with_index_y_crosses_page_flip() {
+            let mut memory: [Byte; 512] = [0x00; 512];
+            memory[0x0000] = INDIRECT_ZERO_PAGE_ADDRESS_PLACE;
+            memory[0x0001] = ADDRESS_LO_ON_ZERO_PAGE_BOUNDARY;
+            memory[0x0002] = ADDRESS_HI;
+            memory[0x0101] = VALUE;
+
+            let mut cpu = CPU::new(Rc::new(RefCell::new(MemoryMock::new(&memory))));
+            cpu.accumulator = 0x07;
+            cpu.index_register_y = 0x02;
+            cpu.program_counter = 0x00;
+            cpu.cycle = 0;
+
+            eor_iny(&mut cpu);
+
+            assert_eq!(cpu.cycle, 5);
+        }
+    }
+}
