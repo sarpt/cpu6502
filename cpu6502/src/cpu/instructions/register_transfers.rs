1f25d9e60c2351318c195f8f74b160ca6b873527
sarpt <mike.m.info@gmail.com>
2024-12-01 16:48:06 +0100

    Change cycle runner to a boxed dyn Fn to allow capturing closure env

--- cpu6502/src/cpu/instructions/register_transfers.rs
+++ cpu6502/src/cpu/instructions/register_transfers.rs
@@ -1,33 +1,33 @@
 use crate::cpu::{Registers, CPU};
 
 pub fn tax(cpu: &mut CPU) {
-    cpu.schedule_cycle(|cpu| {
+    cpu.schedule_cycle(Box::new(|cpu| {
         cpu.transfer_registers(Registers::Accumulator, Registers::IndexX);
-    });
+    }));
 
     cpu.run_next_cycles(1);
 }
 
 pub fn txa(cpu: &mut CPU) {
-    cpu.schedule_cycle(|cpu| {
+    cpu.schedule_cycle(Box::new(|cpu| {
         cpu.transfer_registers(Registers::IndexX, Registers::Accumulator);
-    });
+    }));
 
     cpu.run_next_cycles(1);
 }
 
 pub fn tay(cpu: &mut CPU) {
-    cpu.schedule_cycle(|cpu| {
+    cpu.schedule_cycle(Box::new(|cpu| {
         cpu.transfer_registers(Registers::Accumulator, Registers::IndexY);
-    });
+    }));
 
     cpu.run_next_cycles(1);
 }
 
 pub fn tya(cpu: &mut CPU) {
-    cpu.schedule_cycle(|cpu| {
+    cpu.schedule_cycle(Box::new(|cpu| {
         cpu.transfer_registers(Registers::IndexY, Registers::Accumulator);
-    });
+    }));
 
     cpu.run_next_cycles(1);
 }
--- cpu6502/src/cpu/instructions/stack_operations.rs
+++ cpu6502/src/cpu/instructions/stack_operations.rs
@@ -29,17 +29,17 @@ pub fn plp(cpu: &mut CPU) {
 }
 
 pub fn tsx(cpu: &mut CPU) {
-    cpu.schedule_cycle(|cpu| {
+    cpu.schedule_cycle(Box::new(|cpu| {
         cpu.transfer_registers(Registers::StackPointer, Registers::IndexX);
-    });
+    }));
 
     cpu.run_next_cycles(1);
 }
 
 pub fn txs(cpu: &mut CPU) {
-    cpu.schedule_cycle(|cpu| {
+    cpu.schedule_cycle(Box::new(|cpu| {
         cpu.transfer_registers(Registers::IndexX, Registers::StackPointer);
-    });
+    }));
 
     cpu.run_next_cycles(1);
 }
