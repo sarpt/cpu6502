ac13e9c779c0aa62751e82907d875537af5b76b7
sarpt <mike.m.info@gmail.com>
2024-12-01 14:30:30 +0100

    Separate system functions into cycles queue

--- cpu6502/src/cpu/instructions/system_functions.rs
+++ cpu6502/src/cpu/instructions/system_functions.rs
@@ -4,30 +4,76 @@ use crate::{
 };
 
 pub fn nop(cpu: &mut CPU) {
-    cpu.increment_program_counter();
+    cpu.schedule_cycle(|cpu: &mut CPU| {
+        cpu.queued_increment_program_counter();
+    });
+
+    cpu.run_next_cycles(1);
 }
 
 pub fn brk(cpu: &mut CPU) {
-    cpu.access_memory(cpu.program_counter); // fetch and discard
-    cpu.increment_program_counter();
+    cpu.schedule_cycle(|cpu: &mut CPU| {
+        cpu.access_memory(cpu.program_counter); // fetch and discard
+        cpu.queued_increment_program_counter();
+    });
 
-    cpu.push_word_to_stack(cpu.program_counter);
-    cpu.push_byte_to_stack(cpu.processor_status.into());
-    cpu.program_counter = cpu.fetch_address_from(BRK_INTERRUPT_VECTOR);
+    cpu.schedule_cycle(|cpu: &mut CPU| {
+        cpu.queued_push_byte_to_stack(cpu.get_program_counter_hi());
+    });
 
-    cpu.processor_status.change_break_flag(true);
-    if cpu.chip_variant == ChipVariant::NMOS {
-        return;
-    }
+    cpu.schedule_cycle(|cpu: &mut CPU| {
+        cpu.queued_push_byte_to_stack(cpu.get_program_counter_lo());
+    });
 
-    cpu.processor_status.change_decimal_mode_flag(false);
+    cpu.schedule_cycle(|cpu: &mut CPU| {
+        cpu.queued_push_byte_to_stack(cpu.processor_status.into());
+    });
+
+    cpu.schedule_cycle(|cpu: &mut CPU| {
+        let lo = cpu.access_memory(BRK_INTERRUPT_VECTOR);
+        cpu.set_program_counter_lo(lo);
+    });
+
+    cpu.schedule_cycle(|cpu: &mut CPU| {
+        let hi = cpu.access_memory(BRK_INTERRUPT_VECTOR + 1);
+        cpu.set_program_counter_hi(hi);
+
+        cpu.processor_status.change_break_flag(true);
+        if cpu.chip_variant == ChipVariant::NMOS {
+            return;
+        }
+
+        cpu.processor_status.change_decimal_mode_flag(false);
+    });
+
+    cpu.run_next_cycles(6);
 }
 
 pub fn rti(cpu: &mut CPU) {
-    cpu.dummy_fetch();
-    cpu.processor_status = cpu.pop_byte_from_stack().into();
-    cpu.program_counter = cpu.pop_word_from_stack();
-    cpu.tick();
+    cpu.schedule_cycle(|cpu: &mut CPU| {
+        cpu.access_memory(cpu.program_counter); // fetch and discard
+    });
+
+    // dummy tick, simulate separate stack pointer decrement
+    // second cycle involves decrement of the stack pointer but poping byte from stack in third cycle does it in a single fn call
+    // TODO: dont create dummy cycles, instead of decrementing and poping values in one call separate them into respective cycles
+    cpu.schedule_cycle(|_: &mut CPU| {});
+
+    cpu.schedule_cycle(|cpu: &mut CPU| {
+        cpu.processor_status = cpu.queued_pop_byte_from_stack().into();
+    });
+
+    cpu.schedule_cycle(|cpu: &mut CPU| {
+        let lo = cpu.queued_pop_byte_from_stack();
+        cpu.set_program_counter_lo(lo);
+    });
+
+    cpu.schedule_cycle(|cpu: &mut CPU| {
+        let hi = cpu.queued_pop_byte_from_stack();
+        cpu.set_program_counter_hi(hi);
+    });
+
+    cpu.run_next_cycles(5);
 }
 
 #[cfg(test)]
