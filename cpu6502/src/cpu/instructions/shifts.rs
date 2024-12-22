f7f0b9e9736ac0e59299098e27834973dfd1a67d
sarpt <mike.m.info@gmail.com>
2024-12-22 15:24:50 +0100

    Implement shifts as queued cycles

--- cpu6502/src/cpu/instructions/shifts.rs
+++ cpu6502/src/cpu/instructions/shifts.rs
@@ -1,34 +1,34 @@
 use crate::{
     consts::Byte,
-    cpu::{AddressingMode, Registers, CPU},
+    cpu::{AddressingMode, Registers, ScheduledCycle, TaskCycleVariant, CPU},
 };
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Clone, Copy)]
 enum Directions {
     Left,
     Right,
 }
 
-fn get_rotate_left_cb(carry: bool) -> impl Fn(&u8) -> u8 {
-    return move |value: &u8| {
+fn get_rotate_left_cb(carry: bool) -> Box<dyn Fn(&u8) -> u8> {
+    return Box::new(move |value: &u8| {
         let mod_value = value << 1;
         if !carry {
             return mod_value;
         }
 
         return mod_value | 0b00000001;
-    };
+    });
 }
 
-fn get_rotate_right_cb(carry: bool) -> impl Fn(&u8) -> u8 {
-    return move |value: &u8| {
+fn get_rotate_right_cb(carry: bool) -> Box<dyn Fn(&u8) -> u8> {
+    return Box::new(move |value: &u8| {
         let mod_value = value >> 1;
         if !carry {
             return mod_value;
         }
 
         return mod_value | 0b10000000;
-    };
+    });
 }
 
 fn shift_left_cb(value: &u8) -> u8 {
@@ -39,45 +39,96 @@ fn shift_right_cb(value: &u8) -> u8 {
     return value >> 1;
 }
 
-fn shift(cpu: &mut CPU, addr_mode: AddressingMode, dir: Directions) {
-    let previous_value: Byte;
-    let modified_value: Byte;
+fn op_acc(cpu: &mut CPU, op: Box<dyn Fn(bool) -> Box<dyn Fn(&u8) -> u8>>, dir: Directions) {
+    let mut cycles: Vec<ScheduledCycle> = Vec::new();
 
-    let cb: Box<dyn Fn(&u8) -> u8> = match dir {
-        Directions::Left => Box::new(shift_left_cb),
-        Directions::Right => Box::new(shift_right_cb),
-    };
-    if addr_mode != AddressingMode::Accumulator {
-        let modification_result = cpu.modify_memory(addr_mode, &cb);
+    cycles.push(Box::new(move |cpu| {
+        let previous_value: Byte;
+        let modified_value: Byte;
+        let current_carry = cpu.processor_status.get_carry_flag();
 
-        match modification_result {
-            Some((previous, modified)) => {
-                previous_value = previous;
-                modified_value = modified;
-            }
-            None => panic!("could not shift value in memory"),
-        };
-    } else {
+        let cb = op(current_carry);
+
         previous_value = cpu.get_register(Registers::Accumulator);
         modified_value = cb(&previous_value);
         cpu.accumulator = modified_value;
-        cpu.cycle += 1;
-    }
 
-    let carry = match dir {
-        Directions::Left => previous_value & 0b10000000 > 0,
-        Directions::Right => previous_value & 0b00000001 > 0,
-    };
-    cpu.processor_status.change_carry_flag(carry);
-    cpu.set_status_of_value(modified_value);
+        let new_carry = match dir {
+            Directions::Left => previous_value & 0b10000000 > 0,
+            Directions::Right => previous_value & 0b00000001 > 0,
+        };
+        cpu.processor_status.change_carry_flag(new_carry);
+        cpu.set_status_of_value(modified_value);
+
+        return TaskCycleVariant::Full;
+    }));
+
+    cpu.schedule_instruction(cycles);
+}
+
+fn op_mem(
+    cpu: &mut CPU,
+    addr_mode: AddressingMode,
+    op: Box<dyn Fn(bool) -> Box<dyn Fn(&u8) -> u8>>,
+    dir: Directions,
+) {
+    let mut cycles: Vec<ScheduledCycle> = Vec::new();
+
+    let mut addr_cycles = cpu.queued_get_address(addr_mode);
+    cycles.append(&mut addr_cycles);
+
+    cycles.push(Box::new(|cpu| {
+        let value = cpu.access_memory(cpu.address_output);
+        cpu.set_ctx_lo(value);
+
+        return TaskCycleVariant::Full;
+    }));
+
+    cycles.push(Box::new(move |cpu| {
+        let current_carry = cpu.processor_status.get_carry_flag();
+        let cb = op(current_carry);
+        let value = match cpu.get_current_instruction_ctx() {
+            Some(ctx) => ctx.to_le_bytes()[0],
+            None => panic!("unexpected lack of value in instruction context to modify"),
+        };
+
+        let modified_value = cb(&value);
+        cpu.set_ctx_hi(modified_value);
+
+        return TaskCycleVariant::Full;
+    }));
+
+    cycles.push(Box::new(move |cpu| {
+        let [previous_value, modified_value] = match cpu.get_current_instruction_ctx() {
+            Some(ctx) => ctx.to_le_bytes(),
+            None => panic!("unexpected lack of value in instruction context to modify"),
+        };
+        cpu.put_into_memory(cpu.address_output, modified_value);
+        cpu.set_status_of_value(modified_value);
+
+        let new_carry = match dir {
+            Directions::Left => previous_value & 0b10000000 > 0,
+            Directions::Right => previous_value & 0b00000001 > 0,
+        };
+        cpu.processor_status.change_carry_flag(new_carry);
+
+        return TaskCycleVariant::Full;
+    }));
+
+    cpu.schedule_instruction(cycles);
 }
 
 fn asl(cpu: &mut CPU, addr_mode: AddressingMode) {
-    shift(cpu, addr_mode, Directions::Left);
+    op_mem(
+        cpu,
+        addr_mode,
+        Box::new(|_| Box::new(shift_left_cb)),
+        Directions::Left,
+    );
 }
 
 pub fn asl_acc(cpu: &mut CPU) {
-    asl(cpu, AddressingMode::Accumulator);
+    op_acc(cpu, Box::new(|_| Box::new(shift_left_cb)), Directions::Left);
 }
 
 pub fn asl_zp(cpu: &mut CPU) {
@@ -97,11 +148,20 @@ pub fn asl_ax(cpu: &mut CPU) {
 }
 
 fn lsr(cpu: &mut CPU, addr_mode: AddressingMode) {
-    shift(cpu, addr_mode, Directions::Right);
+    op_mem(
+        cpu,
+        addr_mode,
+        Box::new(|_| Box::new(shift_right_cb)),
+        Directions::Right,
+    );
 }
 
 pub fn lsr_acc(cpu: &mut CPU) {
-    lsr(cpu, AddressingMode::Accumulator);
+    op_acc(
+        cpu,
+        Box::new(|_| Box::new(shift_right_cb)),
+        Directions::Right,
+    );
 }
 
 pub fn lsr_zp(cpu: &mut CPU) {
@@ -120,45 +180,17 @@ pub fn lsr_ax(cpu: &mut CPU) {
     lsr(cpu, AddressingMode::AbsoluteX);
 }
 
-fn rotate(cpu: &mut CPU, addr_mode: AddressingMode, dir: Directions) {
-    let previous_value: Byte;
-    let modified_value: Byte;
-    let current_carry = cpu.processor_status.get_carry_flag();
-
-    let cb: Box<dyn Fn(&u8) -> u8> = match dir {
-        Directions::Left => Box::new(get_rotate_left_cb(current_carry)),
-        Directions::Right => Box::new(get_rotate_right_cb(current_carry)),
-    };
-    if addr_mode != AddressingMode::Accumulator {
-        let modification_result = cpu.modify_memory(addr_mode, &cb);
-        match modification_result {
-            Some((previous, modified)) => {
-                previous_value = previous;
-                modified_value = modified;
-            }
-            None => panic!("could not rotate value in memory"),
-        };
-    } else {
-        previous_value = cpu.get_register(Registers::Accumulator);
-        modified_value = cb(&previous_value);
-        cpu.accumulator = modified_value;
-        cpu.cycle += 1;
-    }
-
-    let new_carry = match dir {
-        Directions::Left => previous_value & 0b10000000 > 0,
-        Directions::Right => previous_value & 0b00000001 > 0,
-    };
-    cpu.processor_status.change_carry_flag(new_carry);
-    cpu.set_status_of_value(modified_value);
-}
-
 fn rol(cpu: &mut CPU, addr_mode: AddressingMode) {
-    rotate(cpu, addr_mode, Directions::Left);
+    op_mem(
+        cpu,
+        addr_mode,
+        Box::new(get_rotate_left_cb),
+        Directions::Left,
+    );
 }
 
 pub fn rol_acc(cpu: &mut CPU) {
-    rol(cpu, AddressingMode::Accumulator);
+    op_acc(cpu, Box::new(get_rotate_left_cb), Directions::Left);
 }
 
 pub fn rol_zp(cpu: &mut CPU) {
@@ -178,11 +210,16 @@ pub fn rol_ax(cpu: &mut CPU) {
 }
 
 fn ror(cpu: &mut CPU, addr_mode: AddressingMode) {
-    rotate(cpu, addr_mode, Directions::Right);
+    op_mem(
+        cpu,
+        addr_mode,
+        Box::new(get_rotate_right_cb),
+        Directions::Right,
+    );
 }
 
 pub fn ror_acc(cpu: &mut CPU) {
-    ror(cpu, AddressingMode::Accumulator);
+    op_acc(cpu, Box::new(get_rotate_right_cb), Directions::Right);
 }
 
 pub fn ror_zp(cpu: &mut CPU) {
