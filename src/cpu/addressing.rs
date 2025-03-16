956d8d739742b3d0ea41b36cd61409a9a28783e7
sarpt <mike.m.info@gmail.com>
2025-03-16 20:27:00 +0100

    Implement indirect addressing tasks

--- src/cpu/addressing.rs
+++ src/cpu/addressing.rs
@@ -409,3 +409,114 @@ impl Tasks for IndexIndirectXAddressingTasks {
         }
     }
 }
+
+enum IndirectStep {
+    IndirectFetchLo,
+    IndirectFetchHi,
+    AddrFixing,
+    MemoryAccessLo,
+    FixedMemoryAccessHi,
+    IncorrectMemoryAccessHi,
+}
+
+pub struct IndirectAddressingTasks {
+    fixed_addressing: bool,
+    done: bool,
+    step: IndirectStep,
+    tgt_addr_lo: Byte,
+    tgt_addr_hi: Byte,
+}
+
+impl IndirectAddressingTasks {
+    pub fn new_fixed_addressing() -> Self {
+        return IndirectAddressingTasks {
+            fixed_addressing: true,
+            done: false,
+            step: IndirectStep::IndirectFetchLo,
+            tgt_addr_lo: Byte::default(),
+            tgt_addr_hi: Byte::default(),
+        };
+    }
+
+    pub fn new_incorrect_addressing() -> Self {
+        return IndirectAddressingTasks {
+            fixed_addressing: false,
+            done: false,
+            step: IndirectStep::IndirectFetchLo,
+            tgt_addr_lo: Byte::default(),
+            tgt_addr_hi: Byte::default(),
+        };
+    }
+}
+
+impl Tasks for IndirectAddressingTasks {
+    fn done(&self) -> bool {
+        return self.done;
+    }
+
+    fn tick(&mut self, cpu: &mut super::CPU) -> (bool, bool) {
+        if self.done {
+            return (false, self.done);
+        }
+
+        match self.step {
+            IndirectStep::IndirectFetchLo => {
+                self.tgt_addr_lo = cpu.access_memory(cpu.program_counter);
+                cpu.increment_program_counter();
+                self.step = IndirectStep::IndirectFetchHi;
+
+                return (true, false);
+            }
+            IndirectStep::IndirectFetchHi => {
+                self.tgt_addr_hi = cpu.access_memory(cpu.program_counter);
+                cpu.increment_program_counter();
+                if self.fixed_addressing {
+                    self.step = IndirectStep::AddrFixing;
+                } else {
+                    self.step = IndirectStep::MemoryAccessLo;
+                }
+
+                return (true, false);
+            }
+            IndirectStep::AddrFixing => {
+                self.step = IndirectStep::MemoryAccessLo;
+
+                return (true, false);
+            }
+            IndirectStep::MemoryAccessLo => {
+                let addr = Word::from_le_bytes([self.tgt_addr_lo, self.tgt_addr_hi]);
+                let addr_lo = cpu.access_memory(addr);
+                cpu.set_address_output_lo(addr_lo);
+
+                if self.fixed_addressing {
+                    self.step = IndirectStep::FixedMemoryAccessHi;
+                } else {
+                    self.step = IndirectStep::IncorrectMemoryAccessHi;
+                }
+
+                return (true, false);
+            }
+            IndirectStep::FixedMemoryAccessHi => {
+                let addr = Word::from_le_bytes([self.tgt_addr_lo, self.tgt_addr_hi]);
+                let addr_hi = cpu.access_memory(addr + 1);
+                cpu.set_address_output_hi(addr_hi);
+
+                self.done = true;
+                return (true, self.done);
+            }
+            IndirectStep::IncorrectMemoryAccessHi => {
+                let addr = Word::from_le_bytes([self.tgt_addr_lo, self.tgt_addr_hi]);
+                let should_incorrectly_jump = self.tgt_addr_lo == 0xFF;
+                let mut target_addr = addr + 1;
+                if should_incorrectly_jump {
+                    target_addr = addr & 0xFF00;
+                };
+                let addr_hi = cpu.access_memory(target_addr);
+                cpu.set_address_output_hi(addr_hi);
+
+                self.done = true;
+                return (true, self.done);
+            }
+        }
+    }
+}
