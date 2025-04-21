3645f71c025d16ae5496a018d4c8c5947080b806
sarpt <mike.m.info@gmail.com>
2025-04-21 11:00:31 +0200

    Implement ModifyRegisterTasks struct

---
+++ src/cpu/tasks/modify_register.rs
@@ -0,0 +1,123 @@
+use crate::{
+    consts::Byte,
+    cpu::{Registers, CPU},
+};
+
+use super::Tasks;
+
+enum ModificationVariant {
+    Inc,
+    Dec,
+    ShiftLeft,
+    ShiftRight,
+    RotateLeft,
+    RotateRight,
+}
+
+pub struct ModifyRegisterTasks {
+    variant: ModificationVariant,
+    register: Registers,
+    value: Byte,
+    done: bool,
+}
+
+impl ModifyRegisterTasks {
+    pub fn new_inc(register: Registers) -> Self {
+        return ModifyRegisterTasks {
+            done: false,
+            register,
+            value: Byte::default(),
+            variant: ModificationVariant::Inc,
+        };
+    }
+
+    pub fn new_dec(register: Registers) -> Self {
+        return ModifyRegisterTasks {
+            done: false,
+            register,
+            value: Byte::default(),
+            variant: ModificationVariant::Dec,
+        };
+    }
+
+    pub fn new_shift_left(register: Registers) -> Self {
+        return ModifyRegisterTasks {
+            done: false,
+            register,
+            value: Byte::default(),
+            variant: ModificationVariant::ShiftLeft,
+        };
+    }
+
+    pub fn new_shift_right(register: Registers) -> Self {
+        return ModifyRegisterTasks {
+            done: false,
+            register,
+            value: Byte::default(),
+            variant: ModificationVariant::ShiftRight,
+        };
+    }
+
+    pub fn new_rotate_left(register: Registers) -> Self {
+        return ModifyRegisterTasks {
+            done: false,
+            register,
+            value: Byte::default(),
+            variant: ModificationVariant::RotateLeft,
+        };
+    }
+
+    pub fn new_rotate_right(register: Registers) -> Self {
+        return ModifyRegisterTasks {
+            done: false,
+            register,
+            value: Byte::default(),
+            variant: ModificationVariant::RotateRight,
+        };
+    }
+}
+
+impl Tasks for ModifyRegisterTasks {
+    fn done(&self) -> bool {
+        self.done
+    }
+
+    fn tick(&mut self, cpu: &mut CPU) -> bool {
+        if self.done() {
+            panic!("tick mustn't be called when done")
+        }
+
+        self.value = cpu.get_register(self.register);
+        match self.variant {
+            ModificationVariant::Inc => self.value = self.value.wrapping_add(1),
+            ModificationVariant::Dec => self.value = self.value.wrapping_sub(1),
+            ModificationVariant::ShiftLeft => {
+                self.value = self.value << 1;
+            }
+            ModificationVariant::ShiftRight => {
+                self.value = self.value >> 1;
+            }
+            ModificationVariant::RotateLeft => {
+                let mod_value = self.value << 1;
+                if !cpu.processor_status.get_carry_flag() {
+                    self.value = mod_value;
+                } else {
+                    self.value = mod_value | 0b00000001;
+                }
+            }
+            ModificationVariant::RotateRight => {
+                let mod_value = self.value >> 1;
+                if !cpu.processor_status.get_carry_flag() {
+                    self.value = mod_value;
+                } else {
+                    self.value = mod_value | 0b10000000;
+                }
+            }
+        }
+
+        cpu.set_register(self.register, self.value);
+
+        self.done = true;
+        return self.done;
+    }
+}
