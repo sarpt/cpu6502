905c9b0d1158227d1cb76435cc1571c89c9ad3cc
sarpt <mike.m.info@gmail.com>
2025-04-21 21:30:58 +0200

    Remove dummy tasks

--- src/cpu/tasks.rs
+++ src/cpu/tasks.rs
@@ -9,21 +9,3 @@ pub trait Tasks {
     fn done(&self) -> bool;
     fn tick(&mut self, cpu: &mut CPU) -> bool;
 }
-
-pub struct DummyTasks {}
-
-impl Default for DummyTasks {
-    fn default() -> Self {
-        return DummyTasks {};
-    }
-}
-
-impl Tasks for DummyTasks {
-    fn done(&self) -> bool {
-        panic!("done status on dummy task mustn't be checked")
-    }
-
-    fn tick(&mut self, _cpu: &mut CPU) -> bool {
-        panic!("tick mustn't be called on dummy tasks")
-    }
-}
