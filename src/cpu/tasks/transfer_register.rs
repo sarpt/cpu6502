07a000ced64b839f4213b97f4856148fe037c0bf
sarpt <mike.m.info@gmail.com>
2025-04-21 12:08:27 +0200

    Use TransferRegisterTasks for txs & tsx

---
+++ src/cpu/tasks/transfer_register.rs
@@ -0,0 +1,34 @@
+use crate::cpu::{Registers, Tasks, CPU};
+
+pub struct TransferRegistersTasks {
+    src: Registers,
+    tgt: Registers,
+    done: bool,
+}
+
+impl TransferRegistersTasks {
+    pub fn new(src: Registers, tgt: Registers) -> Self {
+        return TransferRegistersTasks {
+            src,
+            tgt,
+            done: false,
+        };
+    }
+}
+
+impl Tasks for TransferRegistersTasks {
+    fn done(&self) -> bool {
+        self.done
+    }
+
+    fn tick(&mut self, cpu: &mut CPU) -> bool {
+        if self.done() {
+            panic!("tick mustn't be called when done")
+        }
+
+        cpu.transfer_registers(self.src, self.tgt);
+
+        self.done = true;
+        return self.done;
+    }
+}
