6ba792204f32636f84f60fdf955d7a71d4cb76dc
Sarpt <mike.m.info@gmail.com>
2026-03-21 19:35:13 +0100

    Add print to registers, reorganize debugger structs

--- src/cpu/debugger.rs
+++ src/cpu/debugger.rs
@@ -1,26 +1,17 @@
-use std::{fmt::Display, ops::RangeInclusive};
+use std::ops::RangeInclusive;
 
 use ringbuffer::{AllocRingBuffer, RingBuffer};
 
 use crate::{
   consts::{Byte, DEFAULT_INSTRUCTION_HISTORY_CAPACITY, Word},
   cpu::{
-    CPU,
-    addressing::{AddressingMode, address::Address},
+    CPU, debugger::{debug_instruction_info::DebugInstructionInfo, registers::Registers},
   },
   memory::Memory,
 };
 
-pub struct DebugInstructionInfo {
-  pub addr: Word,
-  pub addr_symbol: Option<String>,
-  pub opcode: Byte,
-  pub name: &'static str,
-  pub starting_cycle: usize,
-  pub target_addr: Option<Address>,
-  pub target_val: Option<Byte>,
-  pub target_symbol: Option<String>,
-}
+pub mod debug_instruction_info;
+pub mod registers;
 
 #[derive(Debug, PartialEq)]
 pub enum Traps {
@@ -43,6 +34,7 @@ pub enum ProbeEvent {
   NextInstruction,
   AddressingDone,
   TrapHit(Traps),
+  MemoryModification((Byte, Byte)),
 }
 
 pub trait Symbols {
@@ -52,13 +44,6 @@ pub trait Symbols {
 pub struct Debugger {
   instructions: AllocRingBuffer<DebugInstructionInfo>,
   traps: Vec<TrapConditions>,
-}
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Registers {
-  pub a: Byte,
-  pub x: Byte,
-  pub y: Byte,
 }
 
 impl Debugger {
@@ -166,96 +151,6 @@ impl Debugger {
 impl Default for Debugger {
   fn default() -> Self {
     Self::new()
-  }
-}
-
-impl Display for DebugInstructionInfo {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    let instruction_address = self
-      .addr_symbol
-      .clone()
-      .unwrap_or(format!("{:#04X}", self.addr));
-    macro_rules! display_debug_info {
-      ($addr_format:literal, $target_addr:ident) => {
-        write!(
-          f,
-          concat!("{}@{}: {} ", $addr_format),
-          self.starting_cycle, instruction_address, self.name, $target_addr
-        )
-      };
-      ($target_addr:literal) => {
-        write!(
-          f,
-          "{}@{}: {} {}",
-          self.starting_cycle, instruction_address, self.name, $target_addr
-        )
-      };
-      () => {
-        write!(
-          f,
-          "{}@{}: {}",
-          self.starting_cycle, instruction_address, self.name
-        )
-      };
-    }
-
-    macro_rules! display_option_debug_info {
-      ($option:expr, $fmt: literal) => {
-        match $option {
-          Some(tgt) => display_debug_info!($fmt, tgt),
-          None => display_debug_info!("?"),
-        }
-      };
-    }
-
-    match &self.target_symbol {
-      Some(target_symbol) => display_debug_info!("{}", target_symbol),
-      None => {
-        let Some(target_addr) = self.target_addr else {
-          return display_debug_info!("?");
-        };
-
-        let Some(mode) = target_addr.mode else {
-          return display_debug_info!("?");
-        };
-
-        match mode {
-          AddressingMode::Implicit => display_debug_info!(),
-          AddressingMode::Immediate => display_option_debug_info!(self.target_val, "#{}"),
-          AddressingMode::Relative => {
-            display_option_debug_info!(target_addr.indirect().map(|v| v as i8), "*{:+}")
-          }
-          AddressingMode::Indirect => {
-            display_option_debug_info!(target_addr.indirect(), "(${:X})")
-          }
-          AddressingMode::ZeroPage => {
-            display_option_debug_info!(target_addr.value(), "${:X}")
-          }
-          AddressingMode::ZeroPageX => {
-            display_option_debug_info!(target_addr.value(), "${:X},X")
-          }
-          AddressingMode::ZeroPageY => {
-            display_option_debug_info!(target_addr.value(), "${:X},Y")
-          }
-          AddressingMode::Absolute => {
-            display_option_debug_info!(target_addr.value(), "${:X}")
-          }
-          AddressingMode::AbsoluteX => {
-            display_option_debug_info!(target_addr.value(), "${:X},X")
-          }
-          AddressingMode::AbsoluteY => {
-            display_option_debug_info!(target_addr.value(), "${:X},Y")
-          }
-          AddressingMode::IndexIndirectX => {
-            display_option_debug_info!(target_addr.indirect(), "(${:X},X)")
-          }
-          AddressingMode::IndirectIndexY => {
-            display_option_debug_info!(target_addr.indirect(), "(${:X}),Y")
-          }
-          AddressingMode::Accumulator => display_debug_info!("A"),
-        }
-      }
-    }
   }
 }
 
---
+++ src/cpu/debugger/debug_instruction_info.rs
@@ -0,0 +1,105 @@
+use std::fmt::Display;
+
+use crate::{consts::{Byte, Word}, cpu::addressing::{AddressingMode, address::Address}};
+
+pub struct DebugInstructionInfo {
+  pub addr: Word,
+  pub addr_symbol: Option<String>,
+  pub opcode: Byte,
+  pub name: &'static str,
+  pub starting_cycle: usize,
+  pub target_addr: Option<Address>,
+  pub target_val: Option<Byte>,
+  pub target_symbol: Option<String>,
+}
+
+impl Display for DebugInstructionInfo {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    let instruction_address = self
+      .addr_symbol
+      .clone()
+      .unwrap_or(format!("{:#04X}", self.addr));
+    macro_rules! display_debug_info {
+      ($addr_format:literal, $target_addr:ident) => {
+        write!(
+          f,
+          concat!("{}@{}: {} ", $addr_format),
+          self.starting_cycle, instruction_address, self.name, $target_addr
+        )
+      };
+      ($target_addr:literal) => {
+        write!(
+          f,
+          "{}@{}: {} {}",
+          self.starting_cycle, instruction_address, self.name, $target_addr
+        )
+      };
+      () => {
+        write!(
+          f,
+          "{}@{}: {}",
+          self.starting_cycle, instruction_address, self.name
+        )
+      };
+    }
+
+    macro_rules! display_option_debug_info {
+      ($option:expr, $fmt: literal) => {
+        match $option {
+          Some(tgt) => display_debug_info!($fmt, tgt),
+          None => display_debug_info!("?"),
+        }
+      };
+    }
+
+    match &self.target_symbol {
+      Some(target_symbol) => display_debug_info!("{}", target_symbol),
+      None => {
+        let Some(target_addr) = self.target_addr else {
+          return display_debug_info!("?");
+        };
+
+        let Some(mode) = target_addr.mode else {
+          return display_debug_info!("?");
+        };
+
+        match mode {
+          AddressingMode::Implicit => display_debug_info!(),
+          AddressingMode::Immediate => display_option_debug_info!(self.target_val, "#{}"),
+          AddressingMode::Relative => {
+            display_option_debug_info!(target_addr.indirect().map(|v| v as i8), "*{:+}")
+          }
+          AddressingMode::Indirect => {
+            display_option_debug_info!(target_addr.indirect(), "(${:X})")
+          }
+          AddressingMode::ZeroPage => {
+            display_option_debug_info!(target_addr.value(), "${:X}")
+          }
+          AddressingMode::ZeroPageX => {
+            display_option_debug_info!(target_addr.value(), "${:X},X")
+          }
+          AddressingMode::ZeroPageY => {
+            display_option_debug_info!(target_addr.value(), "${:X},Y")
+          }
+          AddressingMode::Absolute => {
+            display_option_debug_info!(target_addr.value(), "${:X}")
+          }
+          AddressingMode::AbsoluteX => {
+            display_option_debug_info!(target_addr.value(), "${:X},X")
+          }
+          AddressingMode::AbsoluteY => {
+            display_option_debug_info!(target_addr.value(), "${:X},Y")
+          }
+          AddressingMode::IndexIndirectX => {
+            display_option_debug_info!(target_addr.indirect(), "(${:X},X)")
+          }
+          AddressingMode::IndirectIndexY => {
+            display_option_debug_info!(target_addr.indirect(), "(${:X}),Y")
+          }
+          AddressingMode::Accumulator => display_debug_info!("A"),
+        }
+      }
+    }
+  }
+}
+
