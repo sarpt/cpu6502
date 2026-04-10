eba72f9b6493664bb28c61673f300add10a5f26c
sarpt <mike.m.info@gmail.com>
2026-04-10 23:48:17 +0200

    Add check for cycles in th tests

--- src/cpu/th_tests.rs
+++ src/cpu/th_tests.rs
@@ -14,6 +14,14 @@ fn nmos6502_tests() {
     let specs = load_spec(&filename);
 
     for spec in specs {
+      macro_rules! spec_assert {
+        ($l_val: expr, $r_val: expr, $text: tt) => {
+          let prefix = format_args!($text);
+          let suffix = format_args!("for test \"{}\" in file \"{filename}\"", &spec.name);
+          assert_eq!($l_val, $r_val, "{} {}", prefix, suffix)
+        };
+      }
+
       let mut uut = CPU::new_nmos();
       let mut memory = Generic64kMem::new();
 
@@ -30,34 +38,66 @@ fn nmos6502_tests() {
         memory[addr] = val as u8;
       }
 
-      for _ in spec.cycles {
+      for (idx, cycle) in spec.cycles.iter().enumerate() {
         uut.tick(&mut memory);
+        let Some(last_op) = memory.get_last_operation() else {
+          panic!("unexpected lack of operation on memory after a cycle");
+        };
+
+        let Cycle::Number(cycle_addr) = cycle[0] else {
+          panic!("the first value in cycle information is supposed to be a number");
+        };
+
+        let Cycle::Number(cycle_val) = cycle[1] else {
+          panic!("the second value in cycle information is supposed to be a number");
+        };
+
+        let Cycle::Text(cycl_op) = &cycle[2] else {
+          panic!("the third value in cycle information is supposed to be a string");
+        };
+
+        let addr = match last_op {
+          crate::memory::Operation::Read(addr) => {
+            assert_eq!(
+              cycl_op, "read",
+              "mismatched memory operation during cycle {idx}"
+            );
+            addr
+          }
+          crate::memory::Operation::Write(addr) => {
+            assert_eq!(
+              cycl_op, "write",
+              "mismatched memory operation during cycle {idx}"
+            );
+            addr
+          }
+        };
+        spec_assert!(
+          addr,
+          cycle_addr,
+          "mismatched address access during cycle {idx}"
+        );
+        let val = memory.data[addr as usize];
+        spec_assert!(val, cycle_val as u8, "mismatched value during cycle {idx}");
       }
 
-      assert_eq!(
-        uut.program_counter, spec.final_status.pc,
-        "program counter for test \"{}\" in file \"{filename}\"",
-        spec.name
+      spec_assert!(
+        uut.program_counter,
+        spec.final_status.pc,
+        "program counter mismatch"
       );
-      assert_eq!(
-        uut.accumulator, spec.final_status.a,
-        "accumulator for test \"{}\" in file \"{filename}\"",
-        spec.name
-      );
+      assert_eq!(uut.accumulator, spec.final_status.a, "accumulator mismatch");
       assert_eq!(
         uut.index_register_x, spec.final_status.x,
-        "index register x for test \"{}\" in file \"{filename}\"",
-        spec.name
+        "index register x mismatch"
       );
       assert_eq!(
         uut.index_register_y, spec.final_status.y,
-        "index register y for test \"{}\" in file \"{filename}\"",
-        spec.name
+        "index register y mismatch"
       );
       assert_eq!(
         uut.stack_pointer, spec.final_status.s,
-        "stack pointer for test \"{}\" in file \"{filename}\"",
-        spec.name
+        "stack pointer mismatch"
       );
       assert_eq!(
         format!("{}", uut.processor_status),
@@ -65,15 +105,13 @@ fn nmos6502_tests() {
           "{}",
           std::convert::Into::<ProcessorStatus>::into(spec.final_status.p)
         ),
-        "processor status for test \"{}\" in file \"{filename}\"",
-        spec.name
+        "processor status mismatch"
       );
 
       for [addr, expected_val] in spec.final_status.ram {
         assert_eq!(
           memory[addr], expected_val as u8,
-          "memory val @ addr \"{addr:#04X}\" for test \"{}\" in file \"{filename}\"",
-          spec.name
+          "memory val @ addr \"{addr:#04X}\" mismatch"
         );
       }
     }
